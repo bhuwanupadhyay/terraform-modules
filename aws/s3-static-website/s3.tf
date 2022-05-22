63b18b659c8aa568c7deec84b0968a33d973daad
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-05-22 18:46:47 -0500

    [AWS]: s3 static website

--- aws/s3-static-website/s3.tf
+++ aws/s3-static-website/s3.tf
@@ -21,7 +21,7 @@ JSON
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  policy = templatefile("s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -42,7 +42,7 @@ resource "aws_s3_bucket" "www_bucket" {
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  policy = templatefile("s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   website {
     redirect_all_requests_to = "https://www.${var.domain_name}"
