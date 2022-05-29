33758fb4906a01f143b7a7df3a708324161157f5
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-05-29 12:42:16 -0500

    [LETSENCRYPT]: all cloud

--- letsencrypt-certs/aws/backend.tf
+++ letsencrypt-certs/aws/backend.tf
@@ -6,7 +6,6 @@ terraform {
     }
   }
   required_version = ">= 0.14"
-  backend "s3" {}
 }
 
 provider "aws" { region = var.region }
@@ -14,10 +13,6 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
-locals {
-  secrets      = jsondecode(file(var.local_secret_file))
-  release_name = var.release_name
-}
 
 data "aws_route53_zone" "dns_zone" {
   name = var.root_domain
